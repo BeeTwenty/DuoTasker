49ece570e1efa7fac1f3036f50af6741df4747f7
beetwenty <sindrebergee96@gmail.com>
2023-12-13 12:25:18 +0100

    uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ python manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server..."
-daphne -p 8000 DuoTasker.asgi:application
+uvicorn DuoTasker.asgi:application --host 0.0.0.0 --port 8000
