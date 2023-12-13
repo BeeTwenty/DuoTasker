cfec9e7f63ab06d6709f98ae08d928b6f4e5d229
beetwenty <sindrebergee96@gmail.com>
2023-12-13 13:19:25 +0100

    test

--- base/consumers.py
+++ base/consumers.py
@@ -2,7 +2,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 class TaskListConsumer(AsyncWebsocketConsumer):
-    print("TaskListConsumer")
     async def connect(self):
         await self.channel_layer.group_add("task_list", self.channel_name)
         await self.accept()
@@ -10,10 +9,9 @@ class TaskListConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("task_list", self.channel_name)
 
-    async def receive(self, text_data):
-        # Handle messages received from the WebSocket
-        pass
-
-    async def task_message(self, event):
-        # Call this to send a message to the group
-        await self.send(text_data=json.dumps(event["text"]))
+    async def task_update(self, event):
+        # Send the message to WebSocket
+        await self.send(text_data=json.dumps({
+            "task_id": event["task_id"],
+            "action": event["action"]
+        }))
