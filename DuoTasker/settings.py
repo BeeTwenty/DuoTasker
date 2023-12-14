d3fa4482a8b94aec6e835da9f7eba6508fd8279b
beetwenty <sindrebergee96@gmail.com>
2023-12-14 12:01:37 +0100

    Changelog

--- DuoTasker/settings.py
+++ DuoTasker/settings.py
@@ -145,6 +145,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # assuming 'favicon.ico' and 'manifest.json' are here
+    # other directories if any
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
