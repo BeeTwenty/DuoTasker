3a8ac3a33a84741831ca1a191c4f377fdac4a92c
beetwenty <sindrebergee96@gmail.com>
2024-03-07 18:53:13 +0100

    reset

--- base/models.py
+++ base/models.py
@@ -22,11 +22,9 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     id = models.BigAutoField(primary_key=True)
     completed = models.BooleanField(default=False)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='tasks', null=True)
     
 
     def __str__(self):
         return self.title
-
-
-
+    
